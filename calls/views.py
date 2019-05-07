1018fd30f592b3ed50605c0d5eeb7e712991c0db
André "decko" de Brito <brito.afa@gmail.com>
2019-05-07 14:51:53 -0300

    Add a mock to reply POST calls to Registry API endpoint. Tests passing

--- calls/views.py
+++ calls/views.py
@@ -3,6 +3,14 @@ from rest_framework.response import Response
 
 
 @api_view(['GET', 'POST'])
-def registry_saver(request, **kwargs):
+def registry_saver(request):
 
-    return Response(status=200)
+    if request.method == 'GET':
+        return Response(status=200)
+
+    response = {
+        'job_id': 'job_id',
+        'data': request.data
+    }
+
+    return Response(data=response, status=201)
