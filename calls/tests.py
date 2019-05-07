0e9586ce2adf7e79c4f5c89d3e98fec9a0639684
André "decko" de Brito <brito.afa@gmail.com>
2019-05-07 14:49:04 -0300

    Test a POST call to Registry API endpoint. Tests failing

--- calls/tests.py
+++ calls/tests.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.urls import resolve, reverse_lazy
 
 from rest_framework import status
@@ -26,3 +28,31 @@ def test_registry_API_endpoint_return_200_OK(client):
     response = client.get(url)
 
     assert response.status_code == status.HTTP_200_OK
+
+
+def test_POST_a_call_and_expect_job_id_and_data_posted(client):
+    """
+    Test POST a start call registry and expect a response from API containing
+    a job_id and the data posted.
+    """
+
+    url = reverse_lazy('calls:registry-list')
+
+    timestamp = datetime(2019, 4, 26, 20, 32, 10)
+
+    start_call = {
+            'type': 'start',
+            'timestamp': timestamp.isoformat(),
+            'call_id': 1,
+            'source': '11111111111',
+            'destination': '22222222222',
+    }
+
+    response = client.post(url, start_call, content_type='application/json')
+    response_data = response.json()
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert 'job_id' in response_data
+
+    for item in start_call.items():
+        assert item in response_data['data'].items()
