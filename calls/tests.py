b2b1e20c4c8e4598784ebcf83498c7525dda74ed
André "decko" de Brito <brito.afa@gmail.com>
2019-05-07 14:14:57 -0300

    Test the base URL to registry endpoint. Tests failing

--- calls/tests.py
+++ calls/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.urls import resolve, reverse_lazy
 
-# Create your tests here.
+from rest_framework import status
+
+
+def test_registry_API_endpoint_and_namespace_definition(client):
+    """
+    Test if there is a registry API endpoint and if is defined as "calls"
+    namespace and "registry-list" as his name
+    """
+
+    url = '/api/v1/registry/'
+    resolved = resolve(url)
+
+    assert resolved.namespace == 'calls'\
+        and resolved.url_name == 'registry-list'
+
+
+def test_registry_API_endpoint_return_200_OK(client):
+    """
+    Test if Registry API Endpoint returns 200 Ok HTTP Status.
+    """
+
+    url = reverse_lazy('calls:registry-list')
+
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
