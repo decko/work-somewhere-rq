8226bdee094fedf989af7603085d0ab6e4d92ff4
André "decko" de Brito <brito.afa@gmail.com>
2019-05-07 22:37:15 -0300

    Move a test from calls app to core app. Tests passing

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.urls import resolve
 
-# Create your tests here.
+
+def test_task_API_endpoint_and_namespace_definition(client):
+    """
+    Test if there is a Task API endpoint and if is defined as "core"
+    namespace and "task-list" as his name
+    """
+
+    url = '/api/v1/task/'
+    resolved = resolve(url)
+
+    assert resolved.namespace == 'core'\
+        and resolved.url_name == 'task-list'
