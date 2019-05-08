3debe8a8a19de3674378ebddff5dc7270c0b8e81
André "decko" de Brito <brito.afa@gmail.com>
2019-05-07 23:02:45 -0300

    Add a fixture for a start call registry and refactor some tests. Tests passing

--- calls/tests.py
+++ calls/tests.py
@@ -9,6 +9,20 @@ from rest_framework import status
 pytestmark = pytest.mark.django_db
 
 
+@pytest.fixture
+def start_call_fx():
+    timestamp = datetime(2019, 4, 26, 12, 32, 10)
+    call = {
+            'type': 'start',
+            'timestamp': timestamp.isoformat(),
+            'call_id': 1,
+            'source': '11111111111',
+            'destination': '22222222222',
+    }
+
+    return call
+
+
 def test_registry_API_endpoint_and_namespace_definition(client):
     """
     Test if there is a registry API endpoint and if is defined as "calls"
@@ -34,7 +48,7 @@ def test_registry_API_endpoint_return_200_OK(client):
     assert response.status_code == status.HTTP_200_OK
 
 
-def test_POST_a_call_and_expect_job_id_and_data_posted(client):
+def test_POST_a_call_and_expect_job_id_and_data_posted(client, start_call_fx):
     """
     Test POST a start call registry and expect a response from API containing
     a job_id and the data posted.
@@ -42,37 +56,31 @@ def test_POST_a_call_and_expect_job_id_and_data_posted(client):
 
     url = reverse_lazy('calls:registry-list')
 
-    timestamp = datetime(2019, 4, 26, 20, 32, 10)
-
-    start_call = {
-            'type': 'start',
-            'timestamp': timestamp.isoformat(),
-            'call_id': 1,
-            'source': '11111111111',
-            'destination': '22222222222',
-    }
-
-    response = client.post(url, start_call, content_type='application/json')
+    response = client.post(url, start_call_fx, content_type='application/json')
     response_data = response.json()
 
     assert response.status_code == status.HTTP_201_CREATED
     assert 'job_id' in response_data
 
-    for item in start_call.items():
+    for item in start_call_fx.items():
         assert item in response_data['data'].items()
 
 
-def test_task_API_endpoint_and_namespace_definition(client):
+def test_expect_200Ok_response_GETting_a_job_id_URL(client, start_call_fx):
     """
-    Test if there is a Task API endpoint and if is defined as "core"
-    namespace and "task-list" as his name
+    Test the job_id URL of a start call registry POST.
     """
 
-    url = '/api/v1/task/'
-    resolved = resolve(url)
+    url = reverse_lazy('calls:registry-list')
 
-    assert resolved.namespace == 'core'\
-        and resolved.url_name == 'task-list'
+    response = client.post(url, start_call_fx, content_type='application/json')
+    response_data = response.json()
+
+    task_url = response_data.get('job_id', None)
+
+    task_response = client.get(task_url)
+
+    assert task_response.status_code == status.HTTP_200_OK
 
 
 def test_expect_200Ok_response_GETting_a_job_id_URL(client):
