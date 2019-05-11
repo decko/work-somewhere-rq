7160c05016520d9c7aca21dbea08b298119249aa
André "decko" de Brito <brito.afa@gmail.com>
2019-05-11 11:40:48 -0300

    Test task api endpoint return 404 when a task is not found. Test failing

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,13 @@
+import pytest
+
+from uuid import uuid4
+
 from django.urls import resolve, reverse
 
 from rest_framework import status
+
+
+pytestmark = pytest.mark.django_db
 
 
 def test_if_theres_a_schema_url_for_metadata_and_documentation(client):
@@ -38,3 +45,15 @@ def test_documentation_for_task_view():
     view = resolve(url).func
 
     assert view.__doc__
+
+
+def test_return_404_when_a_Task_is_not_found(client):
+    """
+    Test if a 404 Http status is returned when a Task is not found.
+    """
+
+    url = reverse('core:task-detail', kwargs={'job_id': uuid4()})
+
+    request = client.get(url)
+
+    assert request.status_code == status.HTTP_404_NOT_FOUND
