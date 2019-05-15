5c4eed8ccf12e9f85b6afc6aa093fe65df0135f2
André "decko" de Brito <brito.afa@gmail.com>
2019-05-15 17:47:40 -0300

    Fix documentation about using fixtures on a test.

--- calls/tests.py
+++ calls/tests.py
@@ -271,6 +271,8 @@ def test_post_a_start_registry_with_invalid_phone_number(client, start_call_fx):
     """
     Test POSTing a start registry with invalid phone number and expect
     something
+
+    Test uses start_call_fx fixture
     """
 
     from calls.serializers import RegistrySerializer
