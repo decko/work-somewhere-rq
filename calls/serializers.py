ac62450f52daec2a86d1e2958322452ade05f639
André "decko" de Brito <brito.afa@gmail.com>
2019-05-15 15:57:14 -0300

    Add phone number validation for source and destination fields on RegistrySerializer. Test passing

--- calls/serializers.py
+++ calls/serializers.py
@@ -1,10 +1,33 @@
+import re
+
 from rest_framework import serializers
 
 from .models import Registry
 from .models import Call
 
 
+def validate_number(value):
+    """
+    Check if the 'source' value is a valid brazillian phone number.
+    """
+
+    expression = r'(^[1-9]\d)(\d{8,10})'
+    match = re.match(expression, value)
+
+    if not match:
+        raise serializers.ValidationError(
+            "The number is not a valid phone number")
+
+    return value
+
+
 class RegistrySerializer(serializers.ModelSerializer):
+
+    source = serializers.CharField(
+        required=False, validators=(validate_number,))
+
+    destination = serializers.CharField(
+        required=False, validators=(validate_number,))
 
     class Meta:
         model = Registry
