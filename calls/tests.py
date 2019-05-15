d08b382f7d91c59d748fc20498ef2d1d472ad332
André "decko" de Brito <brito.afa@gmail.com>
2019-05-15 15:56:05 -0300

    Test if the fields "source" and "destination" are being validated as phone numbers. Test failing

--- calls/tests.py
+++ calls/tests.py
@@ -265,3 +265,18 @@ def test_post_a_start_and_stop_registry_and_get_a_call_using_url(client,
 
     assert response.data.get('start_timestamp')
     assert response.data.get('stop_timestamp')
+
+
+def test_post_a_start_registry_with_invalid_phone_number(client, start_call_fx):
+    """
+    Test POSTing a start registry with invalid phone number and expect
+    something
+    """
+
+    from calls.serializers import RegistrySerializer
+
+    start_call_fx['source'] = '01111111111'
+
+    registry = RegistrySerializer(data=start_call_fx)
+
+    assert not registry.is_valid()
