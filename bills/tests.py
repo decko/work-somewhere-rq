eb960e559a467984d0cd9b164afa7845fbb211d7
André "decko" de Brito <brito.afa@gmail.com>
2019-05-16 13:46:05 -0300

    Test if the list endpoint for Bills API return 403 Forbidden. Test failing

--- bills/tests.py
+++ bills/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from rest_framework import status
 
-# Create your tests here.
+
+def test_list_bills_api_endpoint_return_403(client):
+    """
+    Test for GETting the Bills List API Endpoint and expect it
+    to return 403 since it's no one is authorized to get a list
+    from it.
+    """
+
+    url = '/api/v1/bills'
+
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
