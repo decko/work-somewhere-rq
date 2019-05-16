26c97e2ba65b62ae63c3e7d5bcd29f66b5aff07a
André "decko" de Brito <brito.afa@gmail.com>
2019-05-16 16:00:27 -0300

    Add month and latest period information in response of detail view. Test passing

--- bills/views.py
+++ bills/views.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -8,9 +10,15 @@ def bill_view(request, subscriber=None):
     if not subscriber:
         return Response(status=403)
 
+    today = date.today()
+    latest_period = today.replace(
+        year=today.year if today.month > 1 else today.year - 1,
+        month=today.month - 1 if today.month > 1 else 12,
+        day=1).strftime('%h/%Y')
+
     data = {
         'subscriber': subscriber,
-        'period': ''
+        'period': latest_period,
     }
 
     return Response(data=data, status=200)
