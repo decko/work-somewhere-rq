2846ffea92262e207b93e69f495f7bb5f6ff2dde
André "decko" de Brito <brito.afa@gmail.com>
2019-05-16 14:18:04 -0300

    Add a detail view for Bill API Endpoint. Test passing

--- bills/urls.py
+++ bills/urls.py
@@ -6,7 +6,11 @@ from .views import bill_view
 app_name = 'bills'
 
 urlpatterns = [
+    path('bills/<int:subscriber>',
+         bill_view,
+         name='bill-detail'),
+
     path('bills',
          bill_view,
-         name='bill-list')
+         name='bill-list'),
 ]
