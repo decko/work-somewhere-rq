d22e47f963fe0f73f9ca1b45a0a35be5f974b4c4
André "decko" de Brito <brito.afa@gmail.com>
2019-05-20 16:34:51 -0300

    Test for a 'dispatch' method on core.tasks. Test failing

--- core/tests.py
+++ core/tests.py
@@ -143,3 +143,13 @@ def test_for_process_method_on_serviceabstractclass():
     """
 
     assert ServiceAbstractClass.process
+
+
+def test_for_existence_of_a_dispatch_method():
+    """
+    Test for the existence of a dispatch method witch will be used 
+    """
+
+    from core.tasks import dispatch
+
+    assert callable(dispatch)
