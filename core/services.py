863d4fde1e2ba4cd091f7db1124ad5f76406e392
André "decko" de Brito <brito.afa@gmail.com>
2019-05-20 15:29:50 -0300

    Add a list of abstract methods to ServiceAbstractClass. Test passing

--- core/services.py
+++ core/services.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class ServiceAbstractClass(ABC):
@@ -10,6 +10,7 @@ class ServiceAbstractClass(ABC):
     trigger = None
     queue = None
     message = None
+    validation_class = None
 
     def __init__(self):
 
@@ -18,3 +19,62 @@ class ServiceAbstractClass(ABC):
 
         if not self.queue or not isinstance(self.queue, str):
             raise Exception("A queue must be a string and it is needed to propagate the results.")
+
+    @abstractmethod
+    def obtainMessage(self):
+        """
+        Obtains the data needed to be processed.
+
+        self.message could contain any information used to obtain the data
+        or could be the date itself.
+        """
+        pass
+
+    @abstractmethod
+    def validateMessage(self):
+        """
+        Validates the message received.
+
+        Validate the message received using validation class from
+        validation_class attribute.
+        """
+        pass
+
+    @abstractmethod
+    def transformMessage(self):
+        """
+        Transform the validated message into something else needed.
+
+        Transform the message to a new format used by other methods.
+        """
+        pass
+
+    @abstractmethod
+    def persistData(self):
+        """
+        Persist the data into a storage.
+
+        Persist the data into a storage like a Django Model or something else.
+        """
+        pass
+
+    @abstractmethod
+    def propagateResult(self):
+        """
+        Propagate the result into a defined queue.
+
+        Propagate the result into a defined queue to be processed by other
+        Services.
+        """
+        pass
+
+    def process(self):
+        """
+        Run all the methods to process a message and propagate the result.
+        """
+
+        self.obtainMessage()
+        self.validateMessage()
+        self.transformMessage()
+        self.persistData()
+        self.propagateResult()
