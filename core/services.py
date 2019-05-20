a08672296ac876fd5d1fc72b1fad5506d5654128
André "decko" de Brito <brito.afa@gmail.com>
2019-05-20 14:45:30 -0300

    Add constructor validation for 'trigger' attribute. Test passing

--- core/services.py
+++ core/services.py
@@ -11,4 +11,7 @@ class ServiceAbstractClass(ABC):
     queue = None
     message = None
 
-    pass
+    def __init__(self):
+
+        if not self.trigger or not isinstance(self.trigger, str):
+            raise Exception("A trigger must be a string and it is needed to accept any task.")
