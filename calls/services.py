0615de48960437657bbd16905267d8d2c07c8105
André "decko" de Brito <brito.afa@gmail.com>
2019-05-21 15:17:53 -0300

    Add all abstract methods needed to instantiate the RegistryService class. Test passing

--- calls/services.py
+++ calls/services.py
@@ -8,3 +8,24 @@ class RegistryService(ServiceAbstractClass):
 
     trigger = 'registry-service'
     queue = 'registry-service-done'
+
+    def startTask(self):
+        pass
+
+    def obtainMessage(self):
+        pass
+
+    def validateMessage(self):
+        pass
+
+    def transformMessage(self):
+        pass
+
+    def persistData(self):
+        pass
+
+    def propagateResult(self):
+        pass
+
+    def finishTask(self):
+        pass
