6eb8dc51c7001530875004b35c4c8060d3bbb35d
André "decko" de Brito <brito.afa@gmail.com>
2019-05-23 01:38:55 -0300

    Add Docstring as BillService class documentation. Test passing

--- bills/services.py
+++ bills/services.py
@@ -2,4 +2,7 @@ from core.services import ServiceAbstractClass
 
 
 class BillService(ServiceAbstractClass):
+    """
+    BillService is a service responsible for billing consolidated calls.
+    """
     pass
