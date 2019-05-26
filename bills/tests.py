34d4496c7a68ffd967305eceba994d52b8852812
André "decko" de Brito <brito.afa@gmail.com>
2019-05-26 14:18:04 -0300

    Test for call_price_rept property on Bill model. Test failing

--- bills/tests.py
+++ bills/tests.py
@@ -481,3 +481,13 @@ def test_for_Bill_call_start_time_property(bill):
 
     time = bill.start_timestamp.time()
     assert bill.call_start_time == time
+
+
+def test_for_Bill_call_price_rept(bill):
+    """
+    Test for call_price_rept property on a Bill instance, which contains
+    call_price represented in Brazillian Real (R$).
+    """
+
+    call_price = f"R$ {str(bill.call_price).replace('.', ',')}"
+    assert bill.call_price_rept == call_price
