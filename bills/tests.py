6e096c4bf3a9f6a7791f8247b7d16a9b81d98297
André "decko" de Brito <brito.afa@gmail.com>
2019-05-27 20:59:17 -0300

    Add a fixture with a test set of calls to help the tests.

--- bills/tests.py
+++ bills/tests.py
@@ -7,6 +7,8 @@ from decimal import Decimal
 from django.urls import resolve, reverse
 from rest_framework import status
 
+from model_mommy import mommy
+
 from calls.tests import start_call_fx, stop_call_fx
 
 from core.services import ServiceAbstractClass
@@ -15,6 +17,42 @@ from .models import Bill
 from .services import BillService
 
 pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture(scope="session")
+def bills(django_db_setup, django_db_blocker):
+    source = '99988526423'
+    destination = '9933468278'
+
+    calls = (
+        (70, datetime(2019, 4, 26, 21, 57, 13), datetime(2019, 4, 26, 22, 17, 13), Decimal('0.54')),
+        (71, datetime(2017, 12, 11, 15, 7, 13), datetime(2017, 12, 12, 22, 50, 56), Decimal('123.75')),
+        (72, datetime(2017, 12, 12, 22, 47, 56), datetime(2017, 12, 12, 22, 50, 56), Decimal('125.64')),
+        (73, datetime(2017, 12, 12, 21, 57, 13), datetime(2017, 12, 12, 22, 10, 56), Decimal('0.54')),
+        (74, datetime(2017, 12, 12, 4, 57, 13), datetime(2017, 12, 12, 6, 10, 56), Decimal('1.17')),
+        (75, datetime(2017, 12, 13, 21, 57, 13), datetime(2017, 12, 14, 22, 10, 56), Decimal('86.85')),
+        (76, datetime(2017, 12, 12, 15, 7, 58), datetime(2017, 12, 12, 15, 12, 56), Decimal('0.72')),
+        (77, datetime(2018, 2, 28, 21, 57, 13), datetime(2018, 3, 1, 22, 10, 56), Decimal('86.85')),
+    )
+
+    with django_db_blocker.unblock():
+        for call in calls:
+            mommy.make(Bill,
+                       source_call_url=f"/api/v1/calls/{call[0]}",
+                       subscriber=source,
+                       destination=destination,
+                       start_timestamp=call[1],
+                       stop_timestamp=call[2],
+                       call_duration=call[2]-call[1],
+                       call_price=call[3])
+
+    bills = Bill.objects.all()
+
+    yield bills
+
+    with django_db_blocker.unblock():
+        for bill in bills:
+            bill.delete()
 
 
 @pytest.fixture
