a082d8d677e880952cd0d7a99a7a6138fb8499b5
André "decko" de Brito <brito.afa@gmail.com>
2019-05-27 01:34:40 -0300

    Change fields and sources of serialization. Test passing

--- bills/serializers.py
+++ bills/serializers.py
@@ -5,19 +5,13 @@ from .models import Bill
 
 
 class BilledCallSerializer(serializers.ModelSerializer):
-    destination = serializers.CharField(source='call.destination')
-    call_start_date = serializers.SerializerMethodField()
-    call_start_time = serializers.SerializerMethodField()
+    destination = serializers.CharField()
+    call_start_date = serializers.DateField()
+    call_start_time = serializers.TimeField()
     call_duration = serializers.DurationField()
     call_price = serializers.DecimalField(max_digits=14, decimal_places=2)
-
-    def get_call_start_date(self, obj):
-        return obj.call.start_timestamp.date()
-
-    def get_call_start_time(self, obj):
-        return obj.call.start_timestamp.time()
 
     class Meta:
         model = Bill
         fields = ('destination', 'call_start_date', 'call_start_time',
-                 'call_duration', 'call_price')
+                  'call_duration', 'call_price')
