b976bab98254dd6850c426e05304186e765b991c
André "decko" de Brito <brito.afa@gmail.com>
2019-05-27 23:32:40 -0300

    Test for call_price formated value. Test failing

--- bills/test_serializers.py
+++ bills/test_serializers.py
@@ -17,3 +17,17 @@ def test_for_call_duration_value_format(bill):
     formated_time = '0h08m00s'
 
     assert call_duration == formated_time
+
+
+def test_for_call_price_value_format(bill):
+    """
+    Test for call_price value formated. e.g. R$ 3,96
+    """
+
+    serializer = BilledCallSerializer(bill)
+
+    call_price = serializer.data.get('call_price')
+
+    call_price_formated = 'R$ 1,08'
+
+    assert call_price == call_price_formated
