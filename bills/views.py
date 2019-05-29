b29111bca27139447d5e1624fd037871a1461a4b
André "decko" de Brito <brito.afa@gmail.com>
2019-05-28 21:48:35 -0300

    Add current month and year verification. Test passing

--- bills/views.py
+++ bills/views.py
@@ -3,6 +3,7 @@ from datetime import date
 
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
 
 from .serializers import BillSerializer
@@ -11,24 +12,31 @@ from .models import Bill
 
 @api_view(['GET'])
 def bill_view(request, subscriber=None, month_period=None, year_period=None):
+    today = date.today()
+    current_month = today.month
+    current_year = today.year
 
     if not subscriber:
         return Response(status=403)
 
     if not year_period:
-        year_period = date.today().year
+        year_period = current_year
 
     if not month_period:
-        today = date.today()
         month_period = today.replace(
             year=today.year if today.month > 1 else today.year - 1,
             month=today.month - 1 if today.month > 1 else 12,
             day=1).strftime('%h')
 
-    period = f"{month_period}/{year_period}"
+    month_as_int = {abbr.lower(): month for month, abbr in
+                    enumerate(calendar.month_abbr)}.get(month_period.lower(), 0)
 
-    month_as_int = {abbr: month for month, abbr in
-                    enumerate(calendar.month_abbr)}.get(month_period)
+    if month_as_int >= current_month\
+       and year_period >= current_year:
+        raise ParseError(detail=('It\'s only possible to get a telephone bill'
+                                 ' after the reference period has ended.'))
+
+    period = f"{month_period}/{year_period}"
 
     calls = [call for call in Bill.objects.filter(subscriber=subscriber)
              .filter(stop_timestamp__iso_year=year_period)
