c0500eb5dc323361b0050cd71908aa0c65976f0e
André "decko" de Brito <brito.afa@gmail.com>
2019-05-29 00:43:06 -0300

    Refactor task_view into a CBV. Tests passing

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import TaskRetrieveAPIView, task_view
+from .views import TaskListAPIView
+from .views import TaskRetrieveAPIView
 
 
 app_name = 'core'
 
 urlpatterns = [
     path('task/<uuid:job_id>', TaskRetrieveAPIView.as_view(), name='task-detail'),
-    path('task/', task_view, name='task-list'),
+    path('task/', TaskListAPIView.as_view(), name='task-list'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework.decorators import api_view
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 
@@ -10,33 +11,20 @@ from .serializers import TaskSerializer
 
 
 class TaskRetrieveAPIView(RetrieveAPIView):
+    """
+    Retrieve a single Task instance.
+
+    job_id: uuid
+    The identifier to retrieve the Task.
+    """
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     lookup_field = 'job_id'
 
 
-@api_view(['GET'])
-def task_view(request, job_id=None):
+class TaskListAPIView(ListAPIView):
     """
-    Endpoint to retrieve a specific task.
-
-
-    Parameters
-    ----------
-    job_id : uuid, optional
-        The job_id to retrieve the task
+    Endpoint to retrieve all tasks.
     """
-
-    try:
-        task = Task.objects.get(job_id=job_id)
-    except Task.DoesNotExist:
-        raise NotFound('Task not found')
-
-    response = {
-        'job_id': task.get_absolute_url(),
-        'status': task.status,
-        'data': task.data,
-        'result': task.result
-    }
-
-    return Response(data=response, status=200)
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
